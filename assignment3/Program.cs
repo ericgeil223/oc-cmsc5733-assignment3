9c6aa94e6bbc7acbbe422282212f10fab7a19f8f
Eric Geil <ericgeil223@gmail.com>
2021-05-21 16:36:53 -0500

    assignment 3 complete

---
+++ assignment3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using assignment3;
+
+namespace assignment3
+{
+    
+    public static class Program
+    {
+        public static bool InArr (int[] arr, int target)
+        {
+            foreach (int i in arr)
+            {
+                if (target == i)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 4, 5, 6, 1, 2, 3, -2, -1, 0 };
+            int[] search = { 4, 9, 10, -2 };
+            Test unit = new Test(numbers, search);
+            unit.Unittest();
+        }
+    }
+}
